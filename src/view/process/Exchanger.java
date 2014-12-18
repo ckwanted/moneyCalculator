9984ea0e22ec43f9b95170ea2f4822d3814e572e
usuario <usuario@10.22.147.220>
2014-12-18 13:40:16 +0000

    Money Calculator beta 1.1

--- src/view/process/Exchanger.java
+++ src/view/process/Exchanger.java
@@ -6,7 +6,7 @@ import model.Money;
 public class Exchanger {
     
     public static Money exchangeRate(Money money, ExchangeRate exchangeRate) {
-        return new Money(calculateRate(money,exchangeRate), exchangeRate.getFromCurrency());
+        return new Money(calculateRate(money,exchangeRate), exchangeRate.getToCurrency());
     }
 
     private static double calculateRate(Money money, ExchangeRate exchangeRate) {
